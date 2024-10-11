4e3546085383d9b6d5785350b81c0e11000f3e3f
Megidy <byrka.sergiy@gmail.com>
2024-10-11 21:27:21 +0300

    Added DB operation via interfaces

---
+++ services/user/store.go
@@ -0,0 +1,66 @@
+package user
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/API/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db: db,
+	}
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := s.db.Query("select * from users where email=?", email)
+	if err != nil {
+		return nil, err
+	}
+	u := new(types.User)
+	for rows.Next() {
+		u, err = ScanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if u.Id == 0 {
+		return nil, fmt.Errorf("user not found")
+
+	}
+	return u, nil
+}
+
+func (s *Store) GetUserById(id int) (*types.User, error) {
+	return nil, nil
+}
+
+func (s *Store) CreateUser(user types.User) error {
+	_, err := s.db.Exec("INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)", user.FirstName, user.LastName, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+func ScanRowIntoUser(rows *sql.Rows) (*types.User, error) {
+	user := new(types.User)
+	err := rows.Scan(
+		&user.Id,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Password,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}

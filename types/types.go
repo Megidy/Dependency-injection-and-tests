2877f639c5efc00ea4c3b187c2b7cff78bd7cefb
Megidy <byrka.sergiy@gmail.com>
2024-10-13 22:13:34 +0300

    Added Products and Order services types.

--- types/types.go
+++ types/types.go
@@ -2,8 +2,28 @@ package types
 
 type UserStore interface {
 	GetUserByEmail(email string) (*User, error)
-	GetUserById(id int) (*User, error)
+	GetUserById(id float64) (*User, error)
 	CreateUser(user User) error
+}
+
+type ProductStore interface {
+	GetAllProducts() ([]*Product, error)
+	GetProductById(id int) (*Product, error)
+}
+
+type Product struct {
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
+	Price    int    `json:"price"`
+}
+
+type Order struct {
+	UserID  int
+	Product *Product
+}
+type OrderStore interface {
+	CreateOrder(Order) error
 }
 
 type User struct {
@@ -24,4 +44,9 @@ type SignInPayload struct {
 type LogInPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type CreateOrderPayload struct {
+	Id       int `json:"id"`
+	Quantity int `json:"quantity"`
 }

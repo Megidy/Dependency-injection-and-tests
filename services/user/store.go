1a136d7666e9a94618c899857e83c4b8fc2b0116
Megidy <byrka.sergiy@gmail.com>
2024-10-13 17:14:54 +0300

    Changed func GetUserById.

--- services/user/store.go
+++ services/user/store.go
@@ -18,26 +18,45 @@ func NewStore(db *sql.DB) *Store {
 }
 
 func (s *Store) GetUserByEmail(email string) (*types.User, error) {
-	rows, err := s.db.Query("select * from users where email=?", email)
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
 	if err != nil {
 		return nil, err
 	}
+
 	u := new(types.User)
 	for rows.Next() {
-		u, err = ScanRowIntoUser(rows)
+		u, err = scanRowsIntoUser(rows)
 		if err != nil {
 			return nil, err
 		}
 	}
+
 	if u.Id == 0 {
 		return nil, fmt.Errorf("user not found")
-
 	}
+
 	return u, nil
 }
 
 func (s *Store) GetUserById(id int) (*types.User, error) {
-	return nil, nil
+	rows, err := s.db.Query("SELECT * FROM users WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(types.User)
+	for rows.Next() {
+		u, err = scanRowsIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if u.Id == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return u, nil
 }
 
 func (s *Store) CreateUser(user types.User) error {
@@ -49,7 +68,7 @@ func (s *Store) CreateUser(user types.User) error {
 	return nil
 
 }
-func ScanRowIntoUser(rows *sql.Rows) (*types.User, error) {
+func scanRowsIntoUser(rows *sql.Rows) (*types.User, error) {
 	user := new(types.User)
 	err := rows.Scan(
 		&user.Id,

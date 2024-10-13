6a80a9e21d282ca36c92969214b109977611979a
Megidy <byrka.sergiy@gmail.com>
2024-10-13 22:11:28 +0300

    Added store for products.

---
+++ services/product/store.go
@@ -0,0 +1,68 @@
+package product
+
+import (
+	"database/sql"
+
+	"github.com/API/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db: db,
+	}
+}
+
+func (s *Store) GetAllProducts() ([]*types.Product, error) {
+	rows, err := s.db.Query("SELECT * FROM products")
+	if err != nil {
+		return nil, err
+	}
+
+	products := make([]*types.Product, 0)
+	for rows.Next() {
+		p, err := scanRowsIntoProduct(rows)
+		if err != nil {
+			return nil, err
+		}
+
+		products = append(products, p)
+	}
+
+	return products, nil
+}
+
+func scanRowsIntoProduct(rows *sql.Rows) (*types.Product, error) {
+	product := new(types.Product)
+
+	err := rows.Scan(
+		&product.Id,
+		&product.Name,
+		&product.Price,
+		&product.Quantity,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (s *Store) GetProductById(id int) (*types.Product, error) {
+	product := new(types.Product)
+	row, err := s.db.Query("select * from products where id =?", id)
+	if err != nil {
+		return nil, err
+	}
+	for row.Next() {
+		err = row.Scan(&product.Id, &product.Name, &product.Quantity, &product.Price)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return product, nil
+}

776e5ac6124a6e4c2840f07926c36a02ba4db864
Megidy <byrka.sergiy@gmail.com>
2024-10-16 22:56:59 +0300

    Added auto db initialization

---
+++ init.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS api ;
+USE api;
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
+
+
+CREATE TABLE IF NOT EXISTS products(
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    quantity INT ,
+    price INT 
+);
+
+CREATE TABLE IF NOT EXISTS orders(
+    user_id INT ,
+    product_id INT 
+);
+ 

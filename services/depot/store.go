0f4e91492531b5e4124c35bad5bd096b2d4d778e
Megidy <byrka.sergiy@gmail.com>
2024-10-17 21:41:07 +0300

    Added update of order status.

---
+++ services/depot/store.go
@@ -0,0 +1,25 @@
+package depot
+
+import (
+	"database/sql"
+
+	"github.com/API/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db: db,
+	}
+}
+
+func (s *Store) UpdateOrderStatus(order types.Order) error {
+	_, err := s.db.Exec("update orders set status =?", order.Status)
+	if err != nil {
+		return err
+	}
+	return nil
+}

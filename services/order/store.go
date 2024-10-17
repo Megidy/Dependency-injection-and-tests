64c58e633b73e543008c5c5f6fb7a78d27c719f4
Megidy <byrka.sergiy@gmail.com>
2024-10-17 21:44:03 +0300

    Added get order func.

--- services/order/store.go
+++ services/order/store.go
@@ -17,10 +17,25 @@ func NewStore(db *sql.DB) *Store {
 }
 
 func (s *Store) CreateOrder(order types.Order) error {
-	_, err := s.db.Exec("insert into orders values(?,?)", order.UserID, order.Product.Id)
+	_, err := s.db.Exec("insert into orders values(?,?,?)", order.UserID, order.Product.Id, order.Status)
 
 	if err != nil {
 		return err
 	}
 	return nil
+}
+func (s *Store) GetOrder(order types.Order) (*types.Order, error) {
+	row, err := s.db.Query("select * from orders where user_id = ?", order.UserID)
+	if err != nil {
+		return nil, err
+
+	}
+	var o types.Order
+	for row.Next() {
+		err := row.Scan(&o.UserID, &o.Product.Id, &o.Product.Name, &o.Product.Quantity, &o.Product.Price)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &o, nil
 }

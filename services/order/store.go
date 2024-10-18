f50d6d03d2b4f50047d2b494678239b4b3ca9a00
Megidy <byrka.sergiy@gmail.com>
2024-10-18 23:45:12 +0300

    Added GetOrderByUniqueId.

--- services/order/store.go
+++ services/order/store.go
@@ -2,6 +2,7 @@ package order
 
 import (
 	"database/sql"
+	"log"
 
 	"github.com/API/types"
 )
@@ -17,14 +18,14 @@ func NewStore(db *sql.DB) *Store {
 }
 
 func (s *Store) CreateOrder(order types.Order) error {
-	_, err := s.db.Exec("insert into orders values(?,?,?)", order.UserID, order.Product.Id, order.Status)
+	_, err := s.db.Exec("insert into orders values(?,?,?,?,?)", order.Id, order.UserID, order.Product.Id, order.Quantity, order.Status)
 
 	if err != nil {
 		return err
 	}
 	return nil
 }
-func (s *Store) GetOrder(order types.Order) (*types.Order, error) {
+func (s *Store) GetOrderByUserId(order types.Order) (*types.Order, error) {
 	row, err := s.db.Query("select * from orders where user_id = ?", order.UserID)
 	if err != nil {
 		return nil, err
@@ -32,10 +33,46 @@ func (s *Store) GetOrder(order types.Order) (*types.Order, error) {
 	}
 	var o types.Order
 	for row.Next() {
-		err := row.Scan(&o.UserID, &o.Product.Id, &o.Product.Name, &o.Product.Quantity, &o.Product.Price)
+		err := row.Scan(&o.Id, &o.UserID, &o.Product.Id, &o.Product.Name, &o.Product.Quantity, &o.Product.Price)
 		if err != nil {
 			return nil, err
 		}
 	}
 	return &o, nil
+}
+func (s *Store) GetOrderByUniqueId(order types.Order) (types.Order, error) {
+	row, err := s.db.Query("select * from orders where id = ?", order.Id)
+	if err != nil {
+		return types.Order{}, err
+
+	}
+	var o types.Order
+	for row.Next() {
+		err := row.Scan(&o.Id, &o.UserID, &o.Product.Id, &o.Product.Name, &o.Product.Quantity, &o.Product.Price, &o.Status, &o.Quantity)
+		if err != nil {
+			return types.Order{}, err
+		}
+	}
+	return o, nil
+}
+func (s *Store) GetAllUsersOrders(id int) ([]types.Order, error) {
+	var orders []types.Order
+	rows, err := s.db.Query("select id,user_id,quantity,status from orders where user_id=?", id)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("queried")
+	for rows.Next() {
+		var order types.Order
+		err = rows.Scan(&order.Id, &order.UserID, &order.Quantity, &order.Status)
+		log.Println("Scanned")
+		if err != nil {
+			return nil, err
+
+		}
+		log.Println("appended")
+		orders = append(orders, order)
+	}
+	log.Println("returned ")
+	return orders, nil
 }
